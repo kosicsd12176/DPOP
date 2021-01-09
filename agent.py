bf034c5db99d0ac5cd6a1fadde1fe465c0d3c1ca
kosicsd12176 <kstavicsd@gmail.com>
2021-01-09 08:07:30 -0800

    setting correct the list with domains and utilities for agents

--- agent.py
+++ agent.py
@@ -4,16 +4,27 @@ from experiment.packages.variable import Variable
 
 class Agent:
     def __init__(self):
-        self.meetings = []
-        self.variables = []
+        self.meetings_domains = []
+        self.meetings_utilities = []
+        self.variables_domains = []
+        self.variables_utilities = []
+
 
     def set_meeting(self, Meeting):
-        self.meetings.pop(Meeting.get_meeting_utility())
+        domain, utility = Meeting.get_meeting_utility()
+        self.meetings_domains.append(domain)
+        self.meetings_utilities.append(utility)
 
     def set_variable(self, Variable):
-        self.variables.pop(Variable.get_variable_utility())
+        domain, utility = Variable.get_variable_utility()
+        self.variables_domains.append(domain)
+        self.variables_utilities.append(utility)
 
+    def get_meeting_and_utilities(self):
+        return self.meetings_domains, self.meetings_utilities
 
+    def get_variable_and_utilities(self):
+        return self.variables_domains, self.variables_utilities
 
 
 
--- dpop.py
+++ dpop.py
@@ -1,11 +1,44 @@
 from experiment.packages.variable import Variable
 from experiment.packages.meeting import Meeting
+from experiment.agent import Agent
 
-meetings = []
-meeting_one = Meeting(1,50)
-meeting,utility = meeting_one.get_meeting_utility()
-print(meeting_one.get_meeting_utility())
+#create agent
+agent1 = Agent()
 
-meetings.append(meeting_one.get_meeting_utility())
-print(meetings)
+#set agent's first meeting with utility
+meeting = Meeting(1,50)
+agent1.set_meeting(meeting)
+
+#set agent's second meeting with utility
+meeting.set_meeting_utility(2,50)
+agent1.set_meeting(meeting)
+
+#set agent's variable with utility
+variable = Variable(1,1)
+agent1.set_variable(variable)
+
+variable.set_variable_utility(2,2)
+agent1.set_variable(variable)
+
+variable.set_variable_utility(3,3)
+agent1.set_variable(variable)
+
+variable.set_variable_utility(4,4)
+agent1.set_variable(variable)
+
+variable.set_variable_utility(5,0)
+agent1.set_variable(variable)
+
+variable.set_variable_utility(6,5)
+agent1.set_variable(variable)
+
+variable.set_variable_utility(7,10)
+agent1.set_variable(variable)
+
+variable.set_variable_utility(8,20)
+agent1.set_variable(variable)
+print(agent1.get_meeting_and_utilities())
+print(agent1.get_variable())
+
+#for i in agent1.get_variable():
 
