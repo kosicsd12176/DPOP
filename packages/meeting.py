c704c783cd65cbd9368e3ea5f84d00f845e0dda3
kosicsd12176 <kstavicsd@gmail.com>
2021-01-09 10:10:36 -0800

    setting correct the list with domains and utilities for agents

--- packages/meeting.py
+++ packages/meeting.py
@@ -2,13 +2,13 @@
 
 
 class Meeting:
-    def __init__(self, meeting_type, utility):
-        self.meeting = meeting_type
+    def __init__(self, meeting, utility):
+        self.meeting = meeting
         self.utility = utility
 
     def get_meeting_utility(self):
         return  self.meeting, self.utility
 
-    def set_meeting_utility(self, meeting_type, utility):
-        self.meeting = meeting_type
+    def set_meeting_utility(self, meeting, utility):
+        self.meeting = meeting
         self.utility = utility
